a208dcccaa89c8f75d469cb08c8d317dade9c907
Blizihguh <blizihguh@gmail.com>
2025-03-11 06:43:24 -0500

    Finished location fixer

--- file dump/location-fixer/location-fixer.py
+++ file dump/location-fixer/location-fixer.py
@@ -1,13 +1,11 @@
 import xml.etree.ElementTree as etree
+from pathlib import Path
 import re
+import os
 
-# ISCRIPT_DIRECTORY  = ""
-# JESCRIPT_DIRECTORY = ""
-# OUTPUT_DIRECTORY   = ""
-
-test_iscript  = "test-iscript-176029c.txt"
-test_jescript = "test-input-176029c.xml"
-test_output   = "test-output-176029c.xml"
+ISCRIPT_DIRECTORY  = "iscripts\\"
+JESCRIPT_DIRECTORY = "jescripts_valid\\"
+OUTPUT_DIRECTORY   = "output_valid\\"
 
 def get_locations_from_iscript(filename):
 	with open(filename, "r", encoding="utf-8") as f:
@@ -24,8 +22,7 @@ def get_xml_tree_from_jescript(filename):
 
 	return xml_tree
 
-def create_fixed_jescript_file(filename, xml_tree, location_translations):
-	print("Processing " + filename + "...")
+def create_fixed_jescript_file(output_path, xml_tree, location_translations):
 	for location in xml_tree.getroot().findall("location"):
 		# Get the Japanese version of the location name
 		# sjis = location.find("sjis")
@@ -42,10 +39,24 @@ def create_fixed_jescript_file(filename, xml_tree, location_translations):
 		translation = etree.SubElement(location, "ascii")
 		translation.text = translated_name
 
-	with open(filename, "wb") as o:
+	os.makedirs(os.path.dirname(output_path), exist_ok=True)
+	with open(output_path, "wb") as o:
 		o.write( etree.tostring(xml_tree.getroot(), encoding="utf-8") ) 
 
 if __name__ == "__main__":
-	locations = get_locations_from_iscript(test_iscript)
-	xml = get_xml_tree_from_jescript(test_jescript)
-	create_fixed_jescript_file(test_output, xml, locations)
+	xml_files = list(Path(JESCRIPT_DIRECTORY).rglob("*.[xX][mM][lL]")) # Get a list of all xml files in the path recursively
+
+	for filepath in xml_files:
+		print("Processing " + str(filepath) + "...")
+		jescript_name  = os.path.basename(filepath)
+		iscript_name = jescript_name[-11:-4] + ".txt"  # All of the iscript names are 7 hex digits plus ".txt"
+		input_directory = str(os.path.dirname(filepath))
+		output_path = str(filepath).replace(JESCRIPT_DIRECTORY, OUTPUT_DIRECTORY, 1) # Only modify the first occurrence, to prevent errors like /Foo/Foo/ -> /Bar/Bar/
+		
+		try:
+			locations = get_locations_from_iscript(ISCRIPT_DIRECTORY + iscript_name)
+		except:
+			print("!!!!! iscript not found: " + iscript_name)
+		xml_tree = get_xml_tree_from_jescript(filepath)
+
+		create_fixed_jescript_file(output_path, xml_tree, locations)
